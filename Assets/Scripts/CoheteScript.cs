c0d798674440421299716196d153306fb06e5895
Frotolin <mathifogo@gmail.com>
2025-03-27 10:37:02 -0300

    clase 2

---
+++ Assets/Scripts/CoheteScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoheteScript: MonoBehaviour
+{
+    public GameObject fuegoGO;
+    float coheteSpeed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, coheteSpeed, 0);
+       
+    }
+    public void Despegue()
+
+    {
+        fuegoGO.SetActive(true);
+        coheteSpeed = 0.1f;
+
+
+    }
+}
